01d2ecb1c0e97bf02a1497b4934ca30eb8f86b2f
tothkiralyistvan <tothkiralyistvan@gmail.com>
2023-07-03 11:12:03 -0400

    Update code

--- R code.R
+++ R code.R
@@ -16,3 +16,14 @@ View(positivity)
 View(positivity)
 names(positivity)
 summary(positivity, digits = 4)
+
+# Let's fix the miscoded variables one by one
+# Gender
+freq(positivity$gender)
+
+# Verify that gender is numerical
+class(positivity$gender)
+
+# Recode 1 outlier to NA
+positivity <- positivity %>% 
+  mutate(gender = ifelse(gender > 2, NA, gender))
