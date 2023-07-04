a4e049c33efb347b8387184971184d642a3c990c
tothkiralyistvan <tothkiralyistvan@gmail.com>
2023-07-04 11:15:04 -0400

    Update code

--- R code.R
+++ R code.R
@@ -27,3 +27,17 @@ class(positivity$gender)
 # Recode 1 outlier to NA
 positivity <- positivity %>% 
   mutate(gender = ifelse(gender > 2, NA, gender))
+
+# Age
+freq(positivity$new_age)
+class(positivity$age)
+
+# Recode empty cells, textual responses, change variable type, recode outliers
+positivity <- positivity %>%
+  mutate(new_age4 = age) %>% 
+  mutate(new_age4 = case_when(
+    new_age4 == "tizenhet" ~ "17",
+    TRUE ~ as.character(new_age4))) %>%
+  mutate(new_age4 = as.numeric(new_age4)) %>% 
+  mutate(new_age4 = ifelse(new_age4 < 15 | new_age4 > 92, NA, new_age4))
+freq(positivity$new_age4)
